6d2127418b1f79e5e7e234271aa42c8d23c784a0
Domagoj Šarić <dsaritz@gmail.com>
2024-02-19 21:03:22 +0100

    Ranges: + subrange, view_interface.

---
+++ include/std_fix/ranges/view_interface.hpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+///
+/// Workaround for missing ranges functionality
+/// (granular include version)
+/// (uses ranges-v3 as a fallback and assumes it to be in the include path)
+///
+/// (c) Copyright Domagoj Saric.
+///
+///  Use, modification and distribution are subject to the
+///  Boost Software License, Version 1.0. (See accompanying file
+///  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+///
+///  See http://www.boost.org for most recent version.
+///
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <version>
+
+#if __cpp_lib_ranges >= 201911L
+#   include <ranges>
+#else
+#   include <range/v3/view/interface.hpp>
+    namespace std { using namespace ::ranges::cpp20; }
+#endif
