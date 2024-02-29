fe57690ddb0f19bc989b6af00e245945ba9bde5d
Domagoj Šarić <dsaritz@gmail.com>
2024-02-29 13:22:03 +0100

    Ranges: + to.

---
+++ include/std_fix/ranges/to.hpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+///
+/// Workaround for missing ranges functionality
+/// (granular include version)
+/// (uses ranges-v3 as a fallback and assumes it to be in the include path)
+///
+/// (c) Copyright Domagoj Saric.
+///
+///  Use, modification and distribution are subject to the
+///  Boost Software License, Version 1.0. (See accompanying file
+///  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+///
+///  See http://www.boost.org for most recent version.
+///
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <version>
+
+#if __cpp_lib_ranges_to_container >= 202202L
+#   include <ranges>
+#else
+#   include <range/v3/range/conversion.hpp>
+    namespace std { using ranges::to; }
+#endif
