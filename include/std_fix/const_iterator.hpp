0c0464fe9089ac589bd027b5cbf32c495693a1fc
Domagoj Saric <dsaritz@gmail.com>
2024-10-30 20:31:07 +0100

    More const_iterator fixes.

--- include/std_fix/const_iterator.hpp
+++ include/std_fix/const_iterator.hpp
@@ -9,28 +9,36 @@ namespace std
     class basic_const_iterator : private Iter
     {
     public:
-        using iterator_concept = Iter::iterator_concept;
-        using value_type       = Iter::value_type;
-        using difference_type  = Iter::difference_type;
+        using iterator_concept  = Iter::iterator_concept;
+        using iterator_category = Iter::iterator_concept;
+        using value_type        = Iter::value_type const;
+        using difference_type   = Iter::difference_type;
+        using reference         = value_type &;
+        using pointer           = value_type *;
 
         using Iter::Iter;
         basic_const_iterator( Iter const base ) noexcept : Iter( base ) {}
 
-        auto const & operator* () const noexcept { return Iter::operator* (); }
-        auto const * operator->() const noexcept { return Iter::operator->(); }
-        auto const & operator[]( std::size_t const i ) const noexcept { return Iter::operator[]( i ); }
+        reference operator* () const noexcept { return Iter::operator* (); }
+        pointer   operator->() const noexcept { return Iter::operator->(); }
+        reference operator[]( std::size_t const i ) const noexcept { return Iter::operator[]( i ); }
 
-        // constraints not supported on using directives so...
         basic_const_iterator & operator++(   ) noexcept { Iter::operator++(); return *this; }
         basic_const_iterator   operator++(int) noexcept { auto current{ *this }; Iter::operator++(0); return current; }
         basic_const_iterator & operator--(   ) noexcept { Iter::operator--(); return *this; }
         basic_const_iterator   operator--(int) noexcept { auto current{ *this }; Iter::operator--(0); return current; }
 
-        basic_const_iterator & operator+=( auto const x ) noexcept { static_cast<Iter &>( *this ) += x; return *this; }
-        basic_const_iterator & operator-=( auto const x ) noexcept { static_cast<Iter &>( *this ) -= x; return *this; }
+        basic_const_iterator & operator+=( difference_type const x ) noexcept { static_cast<Iter &>( *this ) += x; return *this; }
+        basic_const_iterator & operator-=( difference_type const x ) noexcept { static_cast<Iter &>( *this ) -= x; return *this; }
 
-        basic_const_iterator   operator+ ( auto const x ) noexcept { auto res{ *this }; res += x; return res; }
-        basic_const_iterator   operator- ( auto const x ) noexcept { auto res{ *this }; res -= x; return res; }
+        basic_const_iterator   operator+ ( difference_type const x ) const noexcept { auto res{ *this }; res += x; return res; }
+        basic_const_iterator   operator- ( difference_type const x ) const noexcept { auto res{ *this }; res -= x; return res; }
+
+        difference_type operator-( basic_const_iterator const & other ) const noexcept { return static_cast<Iter const &>( *this ) - static_cast<Iter const &>( other ); }
+
+        friend auto operator+( difference_type const diff, basic_const_iterator const & it ) noexcept { return it + diff; }
+
+        reference operator[]( std::integral auto const x ) const noexcept { return Iter::operator[]( x ); }
 
         bool operator== ( Iter                 const & other ) const noexcept { return static_cast<Iter const &>( *this ) == other; }
         bool operator== ( basic_const_iterator const & other ) const noexcept { return *this == static_cast<Iter const &>( other ); }
